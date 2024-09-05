d6162f35039225b084a08ed0dad033c0a04ef84a
umesh chilukuri <umeshchandra8663@gmail.com>
2024-09-05 14:25:21 +0530

    created movies store

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,17 +1,24 @@
 import React, { useEffect } from 'react'
 import Header from './Header'
 import { API_OPTIONS } from '../utils/constants';
+import { useDispatch } from 'react-redux';
+import { addNowPlayingMovies } from '../utils/MoviesSlice';
 
 const Browse = () => {
 
-  const url = 'https://api.themoviedb.org/3/movie/now_playing?page=1';
+  const dispatch=useDispatch()
 
 
+
+
+
+  
    const getNowPlayingMovies= async ()=>{
-    const data=await fetch(url, API_OPTIONS);
+    const data=await fetch("https://api.themoviedb.org/3/movie/now_playing?page=1", API_OPTIONS);
    
    const json=await data.json();
-   console.log(json)
+   console.log(json.results)
+   dispatch(addNowPlayingMovies(json.results));
    }
 
 useEffect(()=>{
